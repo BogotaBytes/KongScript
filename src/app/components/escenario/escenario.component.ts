63dea2a7a2a2f4e5934e2e3c90742061f9521468
anfegoca <anfegoca@gmail.com>
2023-05-05 11:37:19 -0500

    Creacion escenario

---
+++ src/app/components/escenario/escenario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escenario',
+  templateUrl: './escenario.component.html',
+  styleUrls: ['./escenario.component.scss']
+})
+export class EscenarioComponent {
+
+
+}
